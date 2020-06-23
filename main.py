e8e1024f4c654b1a4b1b3ada0c245b65f2b6b0dc
Nicolas Vanz <nicavanz@hotmail.com>
2020-06-23 19:48:30 -0300

    beggining CustomProcess class

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import psutil as ps
-
+import psutil, time
+from datetime import datetime
+import os
 
 # returns process that most consume memory
-def get_processes():
+'''def get_processes():
 
     # ps.process_iter() returns all process running with all their details
     # here we take the name and memory usage of the top 5 processes that most consume memory
@@ -11,3 +12,33 @@ def get_processes():
 
 for process in get_processes():
     print(process)
+'''
+for p in ps.process_iter():
+    print(ps.disk_usage('/'))
+    break
+
+
+class CustomProcess(psutil.Process):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def try_create_time(self, *args, **kwargs):
+        try:
+            create_time = datetime.fromtimestamp(super().create_time(*args, **kwargs))
+        except OSError:
+            create_time = datetime.fromtimestamp(psutil.boot_time())
+        return create_time
+
+    def try_CPU_cores(self, *args, **kwargs):
+        try:
+            cores = len(super().cpu_affinity(*args, **kwargs))
+        except psutil.AccessDenied:
+            cores = 0
+
+    def try_nice(self, *args, **kwargs):
+        try:
+            nice = int(super().nice(*args, **kwargs))
+        except psutil.AccessDenied:
+            nice = 0
+
+    def try_memory_usage(self, )
