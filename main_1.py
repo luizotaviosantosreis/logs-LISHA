7ae4102a8a8d9ee1ff0e1225e834de8756bc3b73
luizotaviosantosreis <luizotaviosantosreis@gmail.com>
2020-06-23 19:42:38 -0300

    update

--- main_1.py
+++ main_1.py
@@ -7,7 +7,8 @@ def get_processes():
     # ps.process_iter() returns all process running with all their details
     # here we take the name and memory usage of the top 5 processes that most consume memory
     processes = [(proc.info['name'], proc.info['memory_percent']) for proc in sorted(ps.process_iter(['name', 'memory_percent']), key=lambda proc:proc.info['memory_percent'])][-5:]
-    return processes
+    processes2 = [(proc.info['name'], '%.2f %%'%(proc.info['cpu_percent'])) for proc in sorted(ps.process_iter(['name', 'cpu_percent']), key=lambda proc:proc.info['cpu_percent'])][-5:]
+    return processes, processes2
 
 for process in get_processes():
     print(process)
